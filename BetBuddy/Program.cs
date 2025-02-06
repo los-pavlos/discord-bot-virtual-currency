39bd5c171a8814efd6894571d20ef6a9bc97df0c
Pavel Michenka <pavelmichenka373@gmail.com>
2025-02-06 11:59:06 +0100

    pridani navodu

--- BetBuddy/Program.cs
+++ BetBuddy/Program.cs
@@ -10,6 +10,37 @@ using System.Data.SQLite;
 using System.IO;
 using dotenv.net;
 
+
+/*
+ * Discord bot that allows users to play games and earn virtual currency.
+ * Users can check their balance, play coin flip, enter the lottery, claim daily rewards, work, and play rock-paper-scissors.
+ * The bot uses SQLite to store user data.
+ * 
+ * Commands:
+ * - bb money: Check your balance
+ * - bb addmoney <player> <amount>: Add money to a player's balance
+ * - bb cf <bet>: Play coin flip
+ * - bb lottery [amount]: Enter the lottery or view current participants
+ * - bb drawlottery: Draw the lottery winner
+ * - bb daily: Claim your daily reward
+ * - bb work: Work to earn coins
+ * - bb rps <choice> <bet>: Play rock-paper-scissors
+ * 
+ *  add package DSharpPlus
+ *  add package DSharpPlus.CommandsNext
+ *  add package dotenv.net
+ *  add package Microsoft.Data.Sqlite
+ *  add package Newtonsoft.Json
+ * 
+ * To run the bot:
+ * 1. Create a .env file
+ * 2. Add DISCORD_TOKEN=token to the .env file
+ * 3. Run the program
+ */
+
+
+
+
 namespace ForexCastBot
 {
     internal class Program
